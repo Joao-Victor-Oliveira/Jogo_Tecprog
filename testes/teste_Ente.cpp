6f7a851865f3d1b088f22779274a7a4ae5c3f065
Joao-Victor-Oliveira <jvoobmep@gmail.com>
2023-03-30 01:23:43 -0300

    diretorios

---
+++ testes/teste_Ente.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+#include <stdio.h>
+TEST(TesteSoma, integral) {
+    EXPECT_EQ(5, 5);
+}
+
+TEST(TesteSoma, fatorial) {
+    EXPECT_EQ(-5, -5);
+}
+
+
+
--- testes/teste_main.cpp
+++ testes/teste_main.cpp
@@ -1,3 +1,22 @@
 #include <stdio.h>
+#include <gtest/gtest.h>
 
-int main(){printf("hello testes\n");return 0;}
+int Soma(int a, int b) {
+    return a + b;
+}
+
+#include <gtest/gtest.h>
+
+TEST(TesteSoma, SomaPositivos) {
+    EXPECT_EQ(Soma(2, 3), 5);
+}
+
+TEST(TesteSoma, SomaNegativos) {
+    EXPECT_EQ(Soma(-2, -3), -5);
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+

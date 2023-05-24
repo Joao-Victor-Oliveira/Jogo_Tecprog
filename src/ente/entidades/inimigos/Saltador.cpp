f7a097a82614a53bb3286a67413681b916095b51
Joao-Victor-Oliveira <jvoobmep@gmail.com>
2023-05-24 01:29:51 -0300

    restante

--- src/ente/entidades/inimigos/Saltador.cpp
+++ src/ente/entidades/inimigos/Saltador.cpp
@@ -7,7 +7,7 @@ using namespace Entidades;
 #define VX 5.0
 
 Saltador::Saltador(sf::Vector2f pos):Inimigo("../../imagens/inimigos/Saltador.png",pos,sf::Vector2f(30.f,30.f),1){
-    setVelocidade(sf::Vector2f(VX*(rand()%3 -1),0.f));
+    (rand()%2)?setVelocidade(sf::Vector2f(VX,0.f)):setVelocidade(sf::Vector2f(-1*VX,0.f));
 }
 
 Saltador::~Saltador(){
@@ -15,27 +15,54 @@ Saltador::~Saltador(){
 }
 
 void Saltador::colid(Obstaculo* pObs,sf::Vector2f deslocamento){
-    if(deslocamento.y<=deslocamento.x)
-        velocidade.y = -17.f;
-    else
-        velocidade.x = velocidade.x*-1;
+    if(deslocamento.y<=deslocamento.x){
+        if(velocidade.y >=0 ){
+            move(sf::Vector2f(0.f,deslocamento.y*-1));
+            velocidade.y =-17.f;
+        }
+        else{
+            move(sf::Vector2f(0.f,deslocamento.y));
+            velocidade.y =8.f;
+        }
+    }
+    else{
+        (velocidade.x >=0)?move(sf::Vector2f(deslocamento.x*-1,0.f)):move(sf::Vector2f(deslocamento.x,0.f));
+        velocidade.x *= -1;
+    }
 }
 
 void Saltador::colid(Inimigo* pIni,sf::Vector2f deslocamento){
 
     if(deslocamento.y<=deslocamento.x){
-        velocidade.y = -17.f;
+        if(velocidade.y >=0 ){
+            move(sf::Vector2f(0.f,deslocamento.y*-1));
+            velocidade.y =-17.f;
+        }
+        else{
+            move(sf::Vector2f(0.f,deslocamento.y));
+            velocidade.y =8.f;
+        }
     }
-    else
-        velocidade.x = velocidade.x*-1;
+    else{
+        (velocidade.x >=0)?move(sf::Vector2f(deslocamento.x*-1,0.f)):move(sf::Vector2f(deslocamento.x,0.f));
+        velocidade.x *= -1;
+    }
 }
 
 void Saltador::colid(Jogador* pJog,sf::Vector2f deslocamento){
 
     if(deslocamento.y<=deslocamento.x){
-       velocidade.y = -17.f;
+        if(posicao.y > pJog->getPosicao().y){
+            move(sf::Vector2f(0.f,deslocamento.y*-1));
+            velocidade.y =-17.f;
+        }
+        else{
+            move(sf::Vector2f(0.f,deslocamento.y));
+            velocidade.y =8.f;
+        }
     }
     else{
-        velocidade.x = velocidade.x*-1;
+        (velocidade.x >=0)?move(sf::Vector2f(deslocamento.x*-1,0.f)):move(sf::Vector2f(deslocamento.x,0.f));
+        velocidade.x *= -1;
     }
 }
--- src/fases/criadores/CriadorTeste.cpp
+++ src/fases/criadores/CriadorTeste.cpp
@@ -8,10 +8,10 @@ CriadorTeste::CriadorTeste(){}
 CriadorTeste::~CriadorTeste(){}
 
 void CriadorTeste::criarInimigos(Listas::ListaInimigos* li){
-   /* add(li,new Entidades::Saltador(sf::Vector2f(200.f,200.f)));
+    add(li,new Entidades::Saltador(sf::Vector2f(200.f,200.f)));
     add(li,new Entidades::Saltador(sf::Vector2f(300.f,200.f)));
     add(li,new Entidades::Saltador(sf::Vector2f(400.f,200.f)));
-    add(li,new Entidades::Saltador(sf::Vector2f(500.f,200.f)));*/
+    add(li,new Entidades::Saltador(sf::Vector2f(500.f,200.f)));
 }
 
 void CriadorTeste::criarObstaculos(Listas::ListaObstaculos* lo){
